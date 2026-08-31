8aae4263bc323c5c7a72f844ac7a11f4237a7969
Rioreyblue <franciscorey702@gmail.com>
2026-09-01 01:34:06 +0800

    adding rotation for players

--- src/game.py
+++ src/game.py
@@ -13,14 +13,15 @@ GREEN =(0,0,255)
 FPS = 60
 #player variables
 RED_SPACESHIP_IMAGE = pygame.image.load("Assets/spaceship_red.png")
-RED_SPACESHIP = pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACE_SHIP_WIDTH, SPACE_SHIP_HEIGHT))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACE_SHIP_WIDTH, SPACE_SHIP_HEIGHT)), 90)
+
 YELLOW_SPACESHIP_IMAGE = pygame.image.load("Assets/spaceship_yellow.png")
-YELLOW_SPACESHIP = pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACE_SHIP_WIDTH, SPACE_SHIP_HEIGHT))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACE_SHIP_WIDTH, SPACE_SHIP_HEIGHT)), -90)
 
 def draw():
     WIN.blit(BG,(0,0))
-    WIN.blit(RED_SPACESHIP,(0,HEIGHT/2))
-    WIN.blit(YELLOW_SPACESHIP,(1020, HEIGHT/2))
+    WIN.blit(RED_SPACESHIP,(10,HEIGHT/2))
+    WIN.blit(YELLOW_SPACESHIP,(1030, HEIGHT/2))
     pygame.display.update()
 
 def run_game():
