6a61ac0f915c8b057b13cae57357239e19ab3907
Rioreyblue <franciscorey702@gmail.com>
2026-09-01 00:16:32 +0800

    adding placeholder background

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import pygame
 
 WIDTH, HEIGHT = 1080, 720
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Galaxy Fighters")
+BG = pygame.image.load("Assets/space.png",)
+RED = (255,0,0)
+BLUE =(0,255,0)
+GREEN =(0,0,255)
+FPS = 60
+
+def draw():
+    WIN.fill(GREEN)
+    pygame.display.update()
 
 def main():
     
@@ -10,6 +20,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                
+            draw()
+                
+                
     pygame.quit()
 
 if __name__ == "__main__":
